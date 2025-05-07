6718c07b642308e5c71fc389ae43977b68e61a32
msyed2020 <mikailsyed@yahoo.com>
2025-05-07 17:19:55 -0400

    add libs

---
+++ index.js
@@ -0,0 +1,8 @@
+// routing/server u know the drill lol
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const express = require('express');
+const pdf = require('html-pdf');
+
+const path = require('path');
+const fs = require('fs');
